af1a27256302936b6989516019bfb4cbbec94624
StarlightWT <StarlightWT@protonmail.com>
2025-03-01 17:22:11 +0100

    Autostart lib

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,7 +15,9 @@ async fn request_camera_permission(window: tauri::Window) -> Result<(), String> 
     // let has_permission = get_permission_status(&window); // Your function
 
     // if !has_permission {
-        window.emit("request_camera_permission", ()).map_err(|e| e.to_string())?;
+    window
+        .emit("request_camera_permission", ())
+        .map_err(|e| e.to_string())?;
     // }
 
     Ok(())
@@ -24,6 +26,7 @@ async fn request_camera_permission(window: tauri::Window) -> Result<(), String> 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_autostart::init(tauri_plugin_autostart::MacosLauncher::LaunchAgent, Some(vec![])))
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![request_camera_permission])
         .run(tauri::generate_context!())
