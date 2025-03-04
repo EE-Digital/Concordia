4efcdff81f86797c3c566254412a0b19539499d3
StarlightWT <StarlightWT@protonmail.com>
2025-03-04 11:51:50 +0100

    WIP

---
+++ src/components/chat/chatWindow.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import MessageList from "./messageList.svelte";
+
+	const { messages } = $props();
+</script>
+
+<div class="h-full w-full">
+	<MessageList {messages} />
+</div>
--- src/routes/servers/[serverId]/+page.svelte
+++ src/routes/servers/[serverId]/+page.svelte
@@ -4,6 +4,8 @@
 	import Settings from "~icons/lucide/chevron-down";
 	import { loadServer } from "./serverCache";
 	import type { Message } from "../../../types/Message";
+	import MessageList from "../../../components/chat/messageList.svelte";
+	import ChatWindow from "../../../components/chat/chatWindow.svelte";
 	let server: Server | undefined = $state(undefined);
 	let selectedChannel: string | undefined = $state(undefined);
 	let messages: Awaited<Message[]> = $state([]);
@@ -70,29 +72,14 @@
 				<button onclick={() => selectChannel(channel.id)} style={channel.id === selectedChannel ? "background-color: #FFFFFF33;" : ""} class="cursor-pointer w-full rounded py-1 my-1 mx-1">{channel.title}</button>
 			{/each}
 		</div>
-		<div class="h-full w-full">
-			{#if server.channels.length <= 0}
-				<div class="flex flex-col justify-center content-center w-full h-full">
-					<h1 class="text-center font-bold">No channels found</h1>
-					<h2 class="text-center">Please contact the server administrator if you believe this to be wrong</h2>
-				</div>
-			{/if}
-			{#each messages as message}
-				<div class="m-2 p-2 rounded" style="background-color: #ffffff12;">
-					<div class="flex items-center">
-						{#if message.author.profileUrl}
-							<img src={message.author.profileUrl} alt="" />
-						{:else}
-							<div class="w-8 h-8 rounded flex items-center justify-center font-bold bg-purple-700">
-								{message.author.name.slice(0, 2)}
-							</div>
-						{/if}
-						<h1 class="ml-2">{message.author.name}</h1>
-					</div>
-					<p>{message.text}</p>
-				</div>
-			{/each}
-		</div>
+		{#if selectedChannel}
+			<ChatWindow {messages} />
+		{:else}
+			<div class="flex flex-col justify-center content-center w-full h-full">
+				<h1 class="text-center font-bold">No channels found</h1>
+				<h2 class="text-center">Please contact the server administrator if you believe this to be wrong</h2>
+			</div>
+		{/if}
 	</div>
 {/if}
 
