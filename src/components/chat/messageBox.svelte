46561a196cf47787488b3ff753a8625de046f805
StarlitEvelyn <evelyn.rei@protonmail.com>
2025-03-19 12:19:58 +0100

    Profesional message box

--- src/components/chat/messageBox.svelte
+++ src/components/chat/messageBox.svelte
@@ -3,6 +3,10 @@
 	import MessageList from "./messageList.svelte";
 	import apiRequest from "$lib/apiRequest";
 	import type { Server } from "../../types/LocalData";
+	import IconFile from "~icons/lucide/file";
+	import IconImage from "~icons/lucide/image";
+	import IconPool from "~icons/lucide/chart-pie";
+	import IconSwords from "~icons/lucide/swords";
 
 	type Props = {
 		server: Server;
@@ -31,16 +35,42 @@
 
 	const enterCheck = (e: KeyboardEvent) => {
 		if (e.key === "Enter") {
+			e.preventDefault();
 			sendMessage();
 		}
 	};
 </script>
 
-<div class="w-full px-1 flex">
-	<input onkeypress={enterCheck} type="text" bind:value={message} placeholder="Type a message" class="w-full p-2 rounded bg-zinc-800 m-1 text-white" />
-	<button onclick={sendMessage} class="flex flex-col justify-center items-center bg-zinc-800 w-10 h-10 rounded m-1" class:color={message.length > 0}>
-		<SendButton class="text-white " />
-	</button>
+<div class="w-full px-1 fleex pr-5 pt-2">
+	<div class="w-full flex flex-col overflow-hidden">
+		<!-- Message Box -->
+		<div class="w-full flex bg-zinc-800 rounded-lg">
+			<input onkeypress={enterCheck} type="text" bind:value={message} placeholder="Type a message" class="w-full px-4 py-2 text-white outline-0" />
+			<button onclick={sendMessage} class="flex flex-col justify-center items-center bg-zinc-800 w-10 h-10 rounded m-1" class:color={message.length > 0}>
+				<SendButton class="text-white cursor-pointer" />
+			</button>
+		</div>
+
+		<!-- Tools -->
+		<div class="flex gap-2 px-0 py-2">
+			<button class="border-1 border-dashed rounded-full flex px-3 py-1 text-xs gap-2 items-center cursor-pointer text-neutral-300 hover:text-white hover:bg-zinc-900" onclick={() => console.log("Not implemented yet")}>
+				<IconImage />
+				GIF
+			</button>
+			<button class="border-1 border-dashed rounded-full flex px-3 py-1 text-xs gap-2 items-center cursor-pointer text-neutral-300 hover:text-white hover:bg-zinc-900" onclick={() => console.log("Not implemented yet")}>
+				<IconPool />
+				Poll
+			</button>
+			<button class="border-1 border-dashed rounded-full flex px-3 py-1 text-xs gap-2 items-center cursor-pointer text-neutral-300 hover:text-white hover:bg-zinc-900" onclick={() => console.log("Not implemented yet")}>
+				<IconSwords />
+				Game
+			</button>
+			<button class="border-1 border-dashed rounded-full flex px-3 py-1 text-xs gap-2 items-center cursor-pointer text-neutral-300 hover:text-white hover:bg-zinc-900" onclick={() => console.log("Not implemented yet")}>
+				<IconFile />
+				Attachment
+			</button>
+		</div>
+	</div>
 </div>
 
 <style>
